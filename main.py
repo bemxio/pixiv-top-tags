2033e9c85864c2c4e78a137521561f59473120bb
bemxio <bemxiov@protonmail.com>
2023-07-21 08:57:24 +0200

    add the counting script to the repo

---
+++ main.py
@@ -0,0 +1,52 @@
+from typing import Optional
+import json
+import sys
+
+#import matplotlib.pyplot as plt
+import pixivpy3 as pixiv
+
+def get_max_bookmark_id(result: dict) -> Optional[int]:
+    if not result.get("next_url"):
+        return
+
+    params = dict(param.split("=") for param in result["next_url"].split("?")[1].split("&"))
+    value = params.get("max_bookmark_id", None)
+
+    return value
+
+with open("credentials.json", "r", encoding="utf-8") as file:
+    credentials = json.load(file)
+
+api = pixiv.AppPixivAPI()
+api.set_auth(credentials["access_token"], credentials["refresh_token"])
+
+max_bookmark_id = None
+count = {}
+
+while True:
+    result = api.user_bookmarks_illust(int(sys.argv[-1]), max_bookmark_id=max_bookmark_id)
+
+    for illust in result.get("illusts", []):
+        for tag in illust.get("tags", []):
+            if not tag.get("translated_name"):
+                name = tag["name"]
+            else:
+                name = tag["translated_name"]
+
+            if name not in count:
+                count[name] = 0
+
+            count[name] += 1
+
+    max_bookmark_id = get_max_bookmark_id(result)
+
+    if max_bookmark_id is None:
+        break
+
+count.pop("R-18")
+count.pop("R-18G")
+
+count = dict(sorted(count.items(), key=lambda tag: tag[1], reverse=True))
+
+with open("count.json", "w", encoding="utf-8") as file:
+    json.dump(count, file, indent=4, ensure_ascii=False)
